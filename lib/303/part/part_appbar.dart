38400e2632bdea9ce8e87008f9b9e9adb6c5242d
Zal Solmuş <72752991+zalcod@users.noreply.github.com>
2023-08-13 12:11:47 +0300

    Preffered size widget gives error when used with "with" context so I used it with "implements" context.

--- lib/303/part/part_appbar.dart
+++ lib/303/part/part_appbar.dart
@@ -1,6 +1,6 @@
 part of './part_of_learn.dart';
 
-class _PartofAppBar extends StatelessWidget with PreferredSizeWidget {
+class _PartofAppBar extends StatelessWidget implements PreferredSizeWidget {
   const _PartofAppBar({
     Key? key,
   }) : super(key: key);
